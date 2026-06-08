504f7a5c32d1bd6b2595d06d2bffaa840962772a
Serdar <serdar@anatolia.solutions>
2026-06-08 07:32:38 +0300

    shipping price update

--- settings/shipping.js
+++ settings/shipping.js
@@ -4,7 +4,7 @@ function calculateShippingPrice(items) {
     weight += items[i].weight * items[i].quantity;
   }
   weight = weight / 1000;
-  if (weight <= 5) { return 185; }
-  if (weight <= 10) { return 220; }
-  return 280;
+  if (weight <= 5) { return 195; }
+  if (weight <= 10) { return 230; }
+  return 290;
 }
--- site.js
+++ site.js
@@ -4,9 +4,9 @@ for (let i = 0; i < items.length; i++) {
 weight += items[i].weight * items[i].quantity;
 }
 weight = weight / 1000;
-if (weight <= 5) { return 185; }
-if (weight <= 10) { return 220; }
-return 280;
+if (weight <= 5) { return 195; }
+if (weight <= 10) { return 230; }
+return 290;
 }
 (function() {
 let imgs = document.querySelectorAll("img[data-src]");
